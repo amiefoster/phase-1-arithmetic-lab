205bf7628dec5e075fa28bb086892cca59cd6f30
Amie Foster <amie.n.foster@gmail.com>
2021-08-30 14:45:00 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random() + 1)
+
+let num3 = 40
+let num4 = 10
+ 
+let mod = num3 / num4
+
+
+let max = Math.max(1, 10, 15, 20)
